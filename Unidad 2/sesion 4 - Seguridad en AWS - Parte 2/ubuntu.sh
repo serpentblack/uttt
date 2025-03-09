5192f27c0dc4932d318f7e13b52f8bbc68c87bcd
Crhistian Ovalle Gamba <161147591+serpentblack@users.noreply.github.com>
2025-03-09 13:12:43 -0500

    Configurar instancia ubuntu e instalar la cli

---
+++ Unidad 2/sesion 4 - Seguridad en AWS - Parte 2/ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Actualizar los paquetes
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Instalar unzip
+sudo apt install unzip	
+
+#instalacion aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
