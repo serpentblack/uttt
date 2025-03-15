8f115719ca6fbdfbddbe258372b38ccea552cf87
Crhistian Ovalle Gamba <161147591+serpentblack@users.noreply.github.com>
2025-03-15 17:34:49 -0500

    Update ubuntu_aws.sh

--- Unidad 4/sesion 8 - RDS Avanzado/ubuntu_aws.sh
+++ Unidad 4/sesion 8 - RDS Avanzado/ubuntu_aws.sh
@@ -7,6 +7,6 @@ sudo apt-get upgrade -y
 sudo apt install unzip	
 
 #instalacion aws cli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
 sudo ./aws/install
